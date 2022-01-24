b10ae23d8edfc139a9673aae0e7b911c2be88201
shiqianlong <shiqianlong@aliyun.com>
2022-01-24 15:58:53 +0800

    数据库配置

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 # FileName  : config.py
 # Author    : shiqianlong
+
+DB_USERNAME = 'root'
+DB_PASSWORD = 'root'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'zlbbs'
+
+DB_URI = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8mb4' % (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+# 是否跟踪修改
+SQLALCHEMY_TRACK_MODIFICATIONS = False
