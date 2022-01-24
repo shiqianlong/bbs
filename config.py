fa4ddfb7143974dd0fee7c956a1270902cc9e2e8
shiqianlong <shiqianlong@aliyun.com>
2022-01-24 15:49:02 +0800

    项目初始化

---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# FileName  : config.py
+# Author    : shiqianlong
---
+++ exts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# FileName  : exts.py
+# Author    : shiqianlong
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
