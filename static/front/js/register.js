75b3d40db674d2b2c135530ce562534356a97a00
shiqianlong <shiqianlong@aliyun.com>
2022-01-25 20:04:27 +0800

    验证码倒计时实现

--- static/front/js/register.js
+++ static/front/js/register.js
@@ -3,21 +3,39 @@ var RegisterHandle = function () {
 };
 
 RegisterHandle.prototype.listenSendCaptchaEvent = function () {
-    $("#email-captcha-btn").on('click', function (event) {
-        event.preventDefault();
-        var email = $("input[name='email']").val();
-        var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
-        if (!email || !reg.test(email)) {
-            alert('请输入正确格式的邮箱！');
-            return;
-        }
-        zlajax.get({
-            url: '/email/captcha?email=' + email,
-            success: function (data) {
-                console.log(data)
+    var callback = function (event) {
+        $("#email-captcha-btn").on('click', function (event) {
+            var $this = $(this);
+            event.preventDefault();
+            var email = $("input[name='email']").val();
+            var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
+            if (!email || !reg.test(email)) {
+                alert('请输入正确格式的邮箱！');
+                return;
             }
+            zlajax.get({
+                url: '/email/captcha?email=' + email,
+                success: function (data) {
+                    $this.off('click');
+                    $this.attr('disabled', 'disabled');
+                    var countdown = 6;
+                    var interval = setInterval(function () {
+                        if (countdown > 0) {
+                            $this.text(countdown + 's后重新发送')
+                        } else {
+                            $this.text('发送验证码');
+                            $this.attr('disabled', false);
+                            $this.on('click', callback);
+
+                            clearInterval(interval);
+                        }
+                        countdown--;
+                    }, 1000)
+                }
+            })
         })
-    })
+    };
+    $("#email-captcha-btn").on("click", callback())
 };
 
 RegisterHandle.prototype.run = function () {
